baf95f0c24eb640bc5ea09a6db6430f766e8de0c
Daniel Pires <daniel.pires@qub-it.com>
2021-07-14 16:36:19 +0100

    Remove checkDebts condition from domain, this should be an enrolment process configuration

--- src/main/java/org/fenixedu/academic/domain/enrolment/StudentCurricularPlanEnrolmentManager.java
+++ src/main/java/org/fenixedu/academic/domain/enrolment/StudentCurricularPlanEnrolmentManager.java
@@ -45,8 +45,6 @@ public class StudentCurricularPlanEnrolmentManager
             return;
         }
 
-        checkDebts();
-
         if (isResponsiblePersonAllowedToEnrolStudents() || isResponsibleInternationalRelationOffice()) {
             assertAcademicAdminOfficePreConditions();
 
