7ccb0dcbc3fdafb3ffe00f584c2472357f2f51ec
Nadir Amin <nadir.amin@qub-it.com>
2023-08-31 17:47:39 +0100

    Revert last change on AnyCurricularCourseExceptions
    Refactor: Revert change AnyCurricularCourseExceptions should not excluded same DCP courses from exceptions

--- src/main/java/org/fenixedu/academic/domain/curricularRules/executors/ruleExecutors/AnyCurricularCourseExceptionsExecutorLogic.java
+++ src/main/java/org/fenixedu/academic/domain/curricularRules/executors/ruleExecutors/AnyCurricularCourseExceptionsExecutorLogic.java
@@ -151,8 +151,17 @@ public class AnyCurricularCourseExceptionsExecutorLogic extends AbstractCurricul
 
     public static boolean isException(final CompetenceCourse competenceCourse,
             final DegreeCurricularPlan chosenDegreeCurricularPlan, final StudentCurricularPlan studentCurricularPlan) {
-        return Bennu.getInstance().getAnyCurricularCourseExceptionsConfiguration().getCompetenceCoursesSet()
-                .contains(competenceCourse);
+
+        boolean result = false;
+
+        // can only be considered an exception if the user chose other DCP than his own
+        if (chosenDegreeCurricularPlan != studentCurricularPlan.getDegreeCurricularPlan()) {
+
+            result = Bennu.getInstance().getAnyCurricularCourseExceptionsConfiguration().getCompetenceCoursesSet()
+                    .contains(competenceCourse);
+        }
+
+        return result;
     }
 
     static private RuleResult createResultFalse(final AnyCurricularCourseExceptions rule,
