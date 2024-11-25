4238f848657aa349908f8633473e9d64795dccbe
pedrovpmadeira <165657659+pedrovpmadeira@users.noreply.github.com>
2024-11-25 16:03:38 +0000

    Improvement: Add identification document expiration date to registration history report (#17)
    Improvement: Add identification document expiration date to registration history report
    Relates With: #UCP-FENIXEDU-200

--- src/main/java/org/fenixedu/academicextensions/services/registrationhistory/RegistrationHistoryReport.java
+++ src/main/java/org/fenixedu/academicextensions/services/registrationhistory/RegistrationHistoryReport.java
@@ -861,6 +861,12 @@ public class RegistrationHistoryReport implements Comparable<RegistrationHistory
         return person == null ? null : person.getDocumentIdNumber();
     }
 
+    public LocalDate getDocumentIdExpirationDate() {
+        final Person person = getPerson();
+        YearMonthDay expirationDate = person == null ? null : person.getExpirationDateOfDocumentIdYearMonthDay();
+        return expirationDate == null ? null : expirationDate.toLocalDate();
+    }
+
     public String getGender() {
         final Person person = getPerson();
         final Gender gender = person.getGender();
