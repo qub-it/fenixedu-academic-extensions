8836623f741810724605f86b481840ac29fd7613
Pedro Madeira <pedro.madeira@qub-it.com>
2025-06-23 17:48:50 +0100

    Setup dynamicField codes as constants

--- src/main/java/org/fenixedu/academic/domain/degree/ExtendedDegreeInfo.java
+++ src/main/java/org/fenixedu/academic/domain/degree/ExtendedDegreeInfo.java
@@ -18,6 +18,15 @@ import pt.ist.fenixframework.FenixFramework;
 
 public class ExtendedDegreeInfo extends ExtendedDegreeInfo_Base {
 
+    private static final String SCIENTIFIC_AREAS = "scientificAreas";
+    private static final String STUDY_PROGRAMME_DURATION = "studyProgrammeDuration";
+    private static final String STUDY_REGIME = "studyRegime";
+    private static final String STUDY_PROGRAMME_REQUIREMENTS = "studyProgrammeRequirements";
+    private static final String HIGHER_EDUCATION_ACCESS = "higherEducationAccess";
+    private static final String PROFESSIONAL_STATUTES = "professionalStatus";
+    private static final String SUPPLEMENT_EXTRA_INFORMATION = "supplementExtraInformation";
+    private static final String SUPPLEMENT_OTHER_SOURCES = "supplementOtherSources";
+
     public static void setupDeleteListener() {
         FenixFramework.getDomainModel().registerDeletionListener(DegreeInfo.class, degreeInfo -> {
             ExtendedDegreeInfo edi = degreeInfo.getExtendedDegreeInfo();
@@ -68,105 +77,101 @@ public class ExtendedDegreeInfo extends ExtendedDegreeInfo_Base {
 
     @Override
     public LocalizedString getScientificAreas() {
-        return Optional.ofNullable(DynamicField.findField(this.getDegreeInfo(), "scientificAreas"))
-                .map(dF -> dF.getValue(LocalizedString.class)).orElseGet(super::getScientificAreas);
+        return findFieldByCode(SCIENTIFIC_AREAS).map(dF -> dF.getValue(LocalizedString.class))
+                .orElseGet(super::getScientificAreas);
     }
 
     @Override
     public void setScientificAreas(final LocalizedString scientificAreas) {
-        Optional.ofNullable(DynamicField.findField(this.getDegreeInfo(), "scientificAreas"))
-                .ifPresent(dF -> dF.edit(scientificAreas));
+        findFieldByCode(SCIENTIFIC_AREAS).ifPresent(dF -> dF.edit(scientificAreas));
         super.setScientificAreas(scientificAreas);
     }
 
     @Override
     public LocalizedString getStudyRegime() {
-        return Optional.ofNullable(DynamicField.findField(this.getDegreeInfo(), "studyRegime"))
-                .map(dF -> dF.getValue(LocalizedString.class)).orElseGet(super::getStudyRegime);
+        return findFieldByCode(STUDY_REGIME).map(dF -> dF.getValue(LocalizedString.class)).orElseGet(super::getStudyRegime);
     }
 
     @Override
     public void setStudyRegime(final LocalizedString studyRegime) {
-        Optional.ofNullable(DynamicField.findField(this.getDegreeInfo(), "studyRegime")).ifPresent(dF -> dF.edit(studyRegime));
+        findFieldByCode(STUDY_REGIME).ifPresent(dF -> dF.edit(studyRegime));
         super.setStudyRegime(studyRegime);
     }
 
     @Override
     public LocalizedString getStudyProgrammeDuration() {
-        return Optional.ofNullable(DynamicField.findField(this.getDegreeInfo(), "studyProgrammeDuration"))
-                .map(dF -> dF.getValue(LocalizedString.class)).orElseGet(super::getStudyProgrammeDuration);
+        return findFieldByCode(STUDY_PROGRAMME_DURATION).map(dF -> dF.getValue(LocalizedString.class))
+                .orElseGet(super::getStudyProgrammeDuration);
     }
 
     @Override
     public void setStudyProgrammeDuration(final LocalizedString studyProgrammeDuration) {
-        Optional.ofNullable(DynamicField.findField(this.getDegreeInfo(), "studyProgrammeDuration"))
-                .ifPresent(dF -> dF.edit(studyProgrammeDuration));
+        findFieldByCode(STUDY_PROGRAMME_DURATION).ifPresent(dF -> dF.edit(studyProgrammeDuration));
         super.setStudyProgrammeDuration(studyProgrammeDuration);
     }
 
     @Override
     public LocalizedString getStudyProgrammeRequirements() {
-        return Optional.ofNullable(DynamicField.findField(this.getDegreeInfo(), "studyProgrammeRequirements"))
-                .map(dF -> dF.getValue(LocalizedString.class)).orElseGet(super::getStudyProgrammeRequirements);
+        return findFieldByCode(STUDY_PROGRAMME_REQUIREMENTS).map(dF -> dF.getValue(LocalizedString.class))
+                .orElseGet(super::getStudyProgrammeRequirements);
     }
 
     @Override
     public void setStudyProgrammeRequirements(final LocalizedString studyProgrammeRequirements) {
-        Optional.ofNullable(DynamicField.findField(this.getDegreeInfo(), "studyProgrammeRequirements"))
-                .ifPresent(dF -> dF.edit(studyProgrammeRequirements));
+        findFieldByCode(STUDY_PROGRAMME_REQUIREMENTS).ifPresent(dF -> dF.edit(studyProgrammeRequirements));
         super.setStudyProgrammeRequirements(studyProgrammeRequirements);
     }
 
     @Override
     public LocalizedString getHigherEducationAccess() {
-        return Optional.ofNullable(DynamicField.findField(this.getDegreeInfo(), "higherEducationAccess"))
-                .map(dF -> dF.getValue(LocalizedString.class)).orElseGet(super::getHigherEducationAccess);
+        return findFieldByCode(HIGHER_EDUCATION_ACCESS).map(dF -> dF.getValue(LocalizedString.class))
+                .orElseGet(super::getHigherEducationAccess);
     }
 
     @Override
     public void setHigherEducationAccess(final LocalizedString higherEducationAccess) {
-        Optional.ofNullable(DynamicField.findField(this.getDegreeInfo(), "higherEducationAccess"))
-                .ifPresent(dF -> dF.edit(higherEducationAccess));
+        findFieldByCode(HIGHER_EDUCATION_ACCESS).ifPresent(dF -> dF.edit(higherEducationAccess));
         super.setHigherEducationAccess(higherEducationAccess);
     }
 
     @Override
     public LocalizedString getProfessionalStatus() {
-        return Optional.ofNullable(DynamicField.findField(this.getDegreeInfo(), "professionalStatus"))
-                .map(dF -> dF.getValue(LocalizedString.class)).orElseGet(super::getProfessionalStatus);
+        return findFieldByCode(PROFESSIONAL_STATUTES).map(dF -> dF.getValue(LocalizedString.class))
+                .orElseGet(super::getProfessionalStatus);
     }
 
     @Override
     public void setProfessionalStatus(final LocalizedString professionalStatus) {
-        Optional.ofNullable(DynamicField.findField(this.getDegreeInfo(), "professionalStatus"))
-                .ifPresent(dF -> dF.edit(professionalStatus));
+        findFieldByCode(PROFESSIONAL_STATUTES).ifPresent(dF -> dF.edit(professionalStatus));
         super.setProfessionalStatus(professionalStatus);
     }
 
     @Override
     public LocalizedString getSupplementExtraInformation() {
-        return Optional.ofNullable(DynamicField.findField(this.getDegreeInfo(), "supplementExtraInformation"))
-                .map(dF -> dF.getValue(LocalizedString.class)).orElseGet(super::getSupplementExtraInformation);
+        return findFieldByCode(SUPPLEMENT_EXTRA_INFORMATION).map(dF -> dF.getValue(LocalizedString.class))
+                .orElseGet(super::getSupplementExtraInformation);
     }
 
     @Override
     public void setSupplementExtraInformation(final LocalizedString supplementExtraInformation) {
-        Optional.ofNullable(DynamicField.findField(this.getDegreeInfo(), "supplementExtraInformation"))
-                .ifPresent(dF -> dF.edit(supplementExtraInformation));
+        findFieldByCode(SUPPLEMENT_EXTRA_INFORMATION).ifPresent(dF -> dF.edit(supplementExtraInformation));
         super.setSupplementExtraInformation(supplementExtraInformation);
     }
 
     @Override
     public LocalizedString getSupplementOtherSources() {
-        return Optional.ofNullable(DynamicField.findField(this.getDegreeInfo(), "supplementOtherSources"))
-                .map(dF -> dF.getValue(LocalizedString.class)).orElseGet(super::getSupplementOtherSources);
+        return findFieldByCode(SUPPLEMENT_OTHER_SOURCES).map(dF -> dF.getValue(LocalizedString.class))
+                .orElseGet(super::getSupplementOtherSources);
     }
 
     @Override
     public void setSupplementOtherSources(final LocalizedString supplementOtherSources) {
-        Optional.ofNullable(DynamicField.findField(this.getDegreeInfo(), "supplementOtherSources"))
-                .ifPresent(dF -> dF.edit(supplementOtherSources));
+        findFieldByCode(SUPPLEMENT_OTHER_SOURCES).ifPresent(dF -> dF.edit(supplementOtherSources));
         super.setSupplementOtherSources(supplementOtherSources);
+    }
+
+    private Optional<DynamicField> findFieldByCode(final String code) {
+        return Optional.ofNullable(DynamicField.findField(this.getDegreeInfo(), code));
     }
 
     public void delete() {
